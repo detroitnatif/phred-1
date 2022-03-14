dda0b971dc5115a3ec7ca81db54083098b82a18f
Tyler Klimas <tyler.klimas@duke.edu>
2022-03-14 13:11:34 -0400

    updated input fields

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -3,6 +3,11 @@ import { useState } from 'react'
 const AddTask = ({ onAdd }) => {
     const [text, setText] = useState('')
     const [day, setDay] = useState('')
+    const [PH, setPH] = useState('')
+    const [Soil, setSoil] = useState('')
+    const [Nitrate, setNitrate] = useState('')
+    const [Turbidity, setTurbidity] = useState('')
+    const [Image, setImage] = useState('')
     const [reminder, setReminder] = useState(false)
 
     const onSubmit = (e) => {
@@ -13,11 +18,17 @@ const AddTask = ({ onAdd }) => {
             return
         }
 
-        onAdd({ text, day, reminder })
+        onAdd({ text, day, PH, Soil, Nitrate, Turbidity, Image, reminder })
 
         setText('')
         setDay('')
+        setPH('')
+        setSoil('')
+        setNitrate('')
+        setTurbidity('')
         setReminder(false)
+        setImage('')
+
     }
 
     return (
@@ -32,12 +43,48 @@ const AddTask = ({ onAdd }) => {
                 />
             </div>
             <div className='form-control'>
-                <label>Day & Time</label>
+                <label>Date</label>
                 <input
-                    type='text'
-                    placeholder='Add Day & Time'
+                    type='date'
+                    placeholder='What is the Date?'
                     value={day}
                     onChange={(e) => setDay(e.target.value)}
+                />
+            </div>
+            <div className='form-control'>
+                <label>PH Level</label>
+                <input
+                    type='text'
+                    placeholder='Add PH'
+                    value={PH}
+                    onChange={(e) => setPH(e.target.value)}
+                />
+            </div>
+            <div className='form-control'>
+                <label>Soil Quality</label>
+                <input
+                    type='text'
+                    placeholder='Add Soil Quality'
+                    value={Soil}
+                    onChange={(e) => setSoil(e.target.value)}
+                />
+            </div>
+            <div className='form-control'>
+                <label>Nitrate Level</label>
+                <input
+                    type='text'
+                    placeholder='Add Nitrate'
+                    value={Nitrate}
+                    onChange={(e) => setNitrate(e.target.value)}
+                />
+            </div>
+            <div className='form-control'>
+                <label>Turbidity</label>
+                <input id="#slider"
+                    type='range'
+                    placeholder='Add Turbidity'
+                    value={Turbidity}
+                    onChange={(e) => setTurbidity(e.target.value)}
                 />
             </div>
             <div className='form-control form-control-check'>
@@ -47,6 +94,15 @@ const AddTask = ({ onAdd }) => {
                     checked={reminder}
                     value={reminder}
                     onChange={(e) => setReminder(e.currentTarget.checked)}
+                />
+            </div>
+            <div idclassName='form-control'>
+                <label>Image</label>
+                <input
+                    type='image'
+                    placeholder='Add Image'
+                    value={Image}
+                    onChange={(e) => setImage(e.target.value)}
                 />
             </div>
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ const Header = ({ title, onAdd, showAdd }) => {
             {location.pathname === '/' && (
                 <Button
                     color={showAdd ? 'red' : 'green'}
-                    text={showAdd ? 'Close' : 'Add'}
+                    text={showAdd ? 'HOME' : 'ADD NEW ENTRY'}
                     onClick={onAdd}
                 />
             )}
