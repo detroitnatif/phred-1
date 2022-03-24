082e94219f5756385dcbe3f275edffc4317037b0
Tyler Klimas <tyler.klimas@duke.edu>
2022-03-24 12:51:52 -0400

    django

--- django-phred/backend/phred/admin.py
+++ django-phred/backend/phred/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from django.contrib import admin
+from .models import phred
+
+
+class phredAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "date",
+        "PH",
+        "Soil",
+        "Nitrate",
+        "Turbidity",
+        "Image",
+        "Online",
+    )
+
+
 # Register your models here.
+
+admin.site.register(phred, phredAdmin)
--- django-phred/backend/phred/models.py
+++ django-phred/backend/phred/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class phred(models.Model):
+    name = models.CharField(max_length=120)
+    date = models.TextField()
+    PH = models.TextField()
+    Soil = models.TextField()
+    Nitrate = models.TextField()
+    Turbidity = models.TextField()
+    Image = models.TextField()
+    Online = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.title
